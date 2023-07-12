ae2781a7e93c0d29216676ec28c963d3b035aa08
PavelKalugin <kaluginpavel98@gmail.com>
2023-07-12 17:57:17 +0300

    add write tow txt

--- readScript.py
+++ readScript.py
@@ -2,8 +2,13 @@
 
 import re
 
+out_f = ""
+
 with open('/proc/meminfo') as f:
     f_string = f.read()
     f_string = re.sub(r":","",f_string)
     f_string = re.sub(r" 0"," _",f_string)
-    print(f_string)
+    out_f = f_string
+
+with open('result.txt','w+') as f:
+    f.write(out_f)
