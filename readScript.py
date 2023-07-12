6a7cc4a45f15067ff08fd36bf0f22658796907bf
PavelKalugin <kaluginpavel98@gmail.com>
2023-07-12 19:56:29 +0300

    add sort and out

--- readScript.py
+++ readScript.py
@@ -7,8 +7,18 @@ out_f = ""
 with open('/proc/meminfo') as f:
     f_string = f.read()
     f_string = re.sub(r":","",f_string)
-    f_string = re.sub(r" 0"," _",f_string)
-    out_f = f_string
+    f_list = f_string.split('\n')
+    f_dict = {}
+    for i in f_list:
+        try:
+            x = re.search(r" \d+", i)
+            f_dict[i] = int(x.group(0))
+        except:
+            pass
+    sorted_keys = sorted(f_dict, key=f_dict.get,reverse=True)
+    for i in sorted_keys:
+        out_f+=re.sub(r" 0"," _",i)+'\n'
+    print(out_f)
 
 with open('result.txt','w+') as f:
     f.write(out_f)
